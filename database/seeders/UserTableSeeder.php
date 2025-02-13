60b90f1eb75b232c355b4958c29ff10878e13621
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-13 11:20:03 +0700

    menambahkan komentar di seeders rolesTable.php

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,36 +2,44 @@
 
 namespace Database\Seeders;
 
+// Mengimpor model User untuk membuat user baru
 use App\Models\User;
+// Mengimpor Seeder untuk melakukan seeding ke database
 use Illuminate\Database\Seeder;
+// Mengimpor model Role dari Spatie untuk menangani role pengguna
 use Spatie\Permission\Models\Role;
+// Mengimpor model Permission dari Spatie untuk menangani permission pengguna
 use Spatie\Permission\Models\Permission;
+// Mengimpor WithoutModelEvents jika ingin menonaktifkan event model selama seeding
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Menjalankan seeder untuk mengisi data pada tabel.
      */
     public function run(): void
     {
-        //create user
+        // Membuat user baru dengan nama 'zalfa' dan email 'zalfadwi@gmail.com'
+        // Password di-hash dengan bcrypt untuk enkripsi keamanan
         $user = User::create([
-            'name'      => 'zalfa',
-            'email'     => 'zalfadwi@gmail.com',
-            'password'  => bcrypt('password'),
+            'name'      => 'zalfa',                 // Nama user
+            'email'     => 'zalfadwi@gmail.com',    // Email user
+            'password'  => bcrypt('password'),      // Password terenkripsi dengan bcrypt
         ]);
 
-        //get all permissions
+        // Mengambil semua permissions yang ada dari tabel permissions
         $permissions = Permission::all();
 
-        //get role admin
+        // Menemukan role dengan ID 1 (biasanya role admin) dari tabel roles
         $role = Role::find(1);
 
-        //assign permission to role
+        // Menyinkronkan semua permission yang ada dengan role admin
+        // Artinya role admin akan diberikan akses ke semua permission yang ada
         $role->syncPermissions($permissions);
 
-        //assign role to user
+        // Menugaskan role admin ke user yang baru dibuat
+        // User ini sekarang memiliki semua hak akses yang terkait dengan role 'admin'
         $user->assignRole($role);
     }
 }
