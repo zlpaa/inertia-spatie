33728bbfe373a44b6710a282ced5424635b19596
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-13 11:24:59 +0700

    menambahkan komentar di seeders databaseseeders.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,25 @@
 
 namespace Database\Seeders;
 
+// Mengimpor model User untuk keperluan seeding data user (meskipun tidak digunakan langsung di sini)
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// Mengimpor Seeder untuk menjalankan seeding ke database
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Menjalankan seeder untuk mengisi data pada database.
      */
     public function run(): void
     {
+        // Memanggil seeder untuk mengisi tabel 'roles' dengan data role yang diperlukan
         $this->call(RolesTableSeeder::class);
+
+        // Memanggil seeder untuk mengisi tabel 'permissions' dengan data permission yang diperlukan
         $this->call(PermissionsTableSeeder::class);
+
+        // Memanggil seeder untuk mengisi tabel 'users' dengan data user
         $this->call(UserTableSeeder::class);
     }
 }
